53e3e1072c1dc297071f85f904c22a65f935f1f0
zfighters <62249047+zfighters@users.noreply.github.com>
2021-09-05 00:10:08 +0500

    Completed!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -6,10 +7,80 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                XylophoneButton(
+                  keyNo: 1,
+                  color: Colors.red,
+                ),
+                XylophoneButton(
+                  keyNo: 2,
+                  color: Colors.orange,
+                ),
+                XylophoneButton(
+                  keyNo: 3,
+                  color: Colors.yellow,
+                ),
+                XylophoneButton(
+                  keyNo: 4,
+                  color: Colors.green,
+                ),
+                XylophoneButton(
+                  keyNo: 5,
+                  color: Colors.teal,
+                ),
+                XylophoneButton(
+                  keyNo: 6,
+                  color: Colors.blue,
+                ),
+                XylophoneButton(
+                  keyNo: 7,
+                  color: Colors.purple,
+                ),
+              ],
+            ),
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class XylophoneButton extends StatelessWidget {
+  static AudioCache player = AudioCache();
+  static String FILE_NAME = "note";
+  static String EXTENTION = '.wav';
+
+  int keyNo;
+  Color color;
+
+  XylophoneButton({int keyNo, Color color}) {
+    this.keyNo = keyNo;
+    this.color = color;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          player.play("$FILE_NAME${this.keyNo}$EXTENTION");
+        },
+        style: TextButton.styleFrom(
+          backgroundColor: this.color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero,
+            side: BorderSide(
+                color: this.color
+            ),
+          ),
+        ), child: null,
       ),
     );
   }
